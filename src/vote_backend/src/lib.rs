110da1f4b042042ad594d8fd5767a2552f93b7b5
kozanakyel <kozanakyel@gmail.com>
2023-11-22 20:14:21 +0300

    add dependencies

--- src/vote_backend/src/lib.rs
+++ src/vote_backend/src/lib.rs
@@ -1,4 +1,4 @@
-#[ic_cdk::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
-}
+use candid::{CandidType, Decode, Deserialize, Encode};
+use ic_stable_structures::memory_manager::{MemoryId, MemoryManager, VirtualMemory};
+use ic_stable_structures::{BoundedStorable, DefaultMemoryImpl, StableBTreeMap, Storable};
+use std::{borrow::Cow, cell::RefCell};
